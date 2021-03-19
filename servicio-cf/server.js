28382fd5c28a3d5f7933658bf0a952a53a232224
Alonso Lizaola <alonso@on247.me>
2021-03-19 00:40:41 -0600

    Peticion a API de TA

--- servicio-cf/server.js
+++ servicio-cf/server.js
@@ -2,6 +2,9 @@ const express = require("express"),
     app = express();
 
 const process = require("process");
+
+const axios=require("axios");
+
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL
 
@@ -11,6 +14,23 @@ app.use(express.json())
 app.get("/autor", (req, res) => {
    res.json({"autor":"Alonso Javier Lizaola Salazar"})
 });
+
+app.post("/analisis", async (req, res)=> {
+    if(!Object.keys(req.body).includes("texto")){
+        return res.json({"error":"falta el parametro texto"})
+    }
+    if(req.body.texto==""){
+        return res.json({"error":"Texto vacio"})
+    }
+    let ta_req_data = {"text":req.body.texto};
+    let auth={username:"apikey",password:API_KEY}
+    try{
+        ta_response = await axios.post(API_URL+"/v3/tone?version=2017-09-21",ta_req_data,{auth})
+    }
+    catch(e){
+        return res.json({"error":"Fallo peticion a TA con el error:"+e.response.data.error})
+    }
+ });
 
 app.listen(port);
 console.log("Listening on port ", port);
