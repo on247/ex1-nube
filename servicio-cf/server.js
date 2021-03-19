8983a57840d1f7d2410df48f73cf8ed7f98304ea
Alonso Lizaola <alonso@on247.me>
2021-03-18 23:56:58 -0600

    dependencias / variables entorno
    Añadir dependencias / vairables entorno

--- servicio-cf/server.js
+++ servicio-cf/server.js
@@ -1,11 +1,18 @@
-var express = require("express"),
+const express = require("express"),
     app = express();
 
-var port = process.env.PORT || 8080;
+const process = require("process");
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL
 
+const port = process.env.PORT || 8080;
+
+app.use(express.json())
 app.get("/autor", (req, res) => {
    res.json({"autor":"Alonso Javier Lizaola Salazar"})
 });
 
 app.listen(port);
 console.log("Listening on port ", port);
+console.log("API_KEY=",API_KEY)
+console.log("API_URL=",API_URL)
